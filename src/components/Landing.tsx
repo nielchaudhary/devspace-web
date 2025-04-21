f5ff5d917f0df202402dc7f9341886f4990f4995
Neel Chaudhary <neilchaudhary12@gmail.com>
2025-04-22 03:58:11 +0530

    fix: formatting

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,11 +1,10 @@
-import { GlobeComponent } from "./Globe"
+import { GlobeComponent } from './Globe';
 
 export const Landing = () => {
   return (
     <div className="flex flex-col items-center justify-start pt-20 h-screen bg-black w-full relative mt-10">
-        <h1/>
-        <GlobeComponent />
-      
+      <h1 />
+      <GlobeComponent />
     </div>
-  )
-}
+  );
+};
