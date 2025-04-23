90130bd941399a71ad5d20e30db98dbbb40868e9
Neel Chaudhary <neilchaudhary12@gmail.com>
2025-04-24 01:49:22 +0530

    added stars background consitently across all the pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { pageRoutes } from './router';
+import { StarsBackground } from './components/Stars';
 
 function App() {
   return (
     <BrowserRouter>
+      <StarsBackground className="absolute inset-0 z-0" />
       <Routes>
         {pageRoutes.map((pageRoute) => (
           <Route key={pageRoute.path} path={pageRoute.path} element={<pageRoute.component />} />
