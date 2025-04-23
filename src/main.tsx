3e4cb615ce9b1fbdaa35c0d9fe19e40a720f3fe0
Neel Chaudhary <neilchaudhary12@gmail.com>
2025-04-24 01:42:41 +0530

    added ClerkAuthProvider for login setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ClerkProvider } from '@clerk/clerk-react';
+
 import './index.css';
 import App from './App';
+import { isNullOrUndefined } from './utils/data-helpers';
+
+const VITE_CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (isNullOrUndefined(VITE_CLERK_PUBLISHABLE_KEY)) {
+  throw new Error('Missing Clerk Auth Public Key');
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={VITE_CLERK_PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </StrictMode>
 );
